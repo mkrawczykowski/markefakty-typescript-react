31f3c141d0985f627403c75454cb1e5c71bfb537
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-12-05 09:11:28 +0100

    something doesn't work here. An empty page instead of <FirstComponent>

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import * as ReactDOM from "react-dom/client"
 import FirstComponent from './components/FirstComponent'
 import Layout from './components/structure/Layout/Layout';
 
+// import './styles/index.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Layout>
-        test
+        <FirstComponent></FirstComponent>
     </Layout>
   </React.StrictMode>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,3 @@ module.exports = {
     },
     mode: 'development'
 };
-
